52297175b4a608718cf0e90e0ee4fd71ad30ab67
Michael Knudsen <micknudsen@gmail.com>
2020-02-18 20:02:51 +0100

    Test that attempting to get logr for uncovered position raises exception

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,6 +3,8 @@ import unittest
 from gatk2ascat.core import Segment
 from gatk2ascat.core import Segmentation
 
+from gatk2ascat.exceptions import UncoveredPositionError
+
 
 class TestSegment(unittest.TestCase):
 
@@ -26,8 +28,12 @@ class TestSegmentation(unittest.TestCase):
         self.assertEqual(self.segmentation._segments['chr1'], [self.segment_1a, self.segment_1b])
         self.assertEqual(self.segmentation._segments['chr2'], [self.segment_2a])
 
-    def test_get_logr(self):
+    def test_get_logr_for_covered_position(self):
         self.assertEqual(self.segmentation.logr(chromosome='chr1', position='100'), 0.5)
         self.assertEqual(self.segmentation.logr(chromosome='chr1', position='200'), 0.5)
         self.assertEqual(self.segmentation.logr(chromosome='chr1', position='300'), 1.2)
         self.assertEqual(self.segmentation.logr(chromosome='chr2', position='400'), -0.7)
+
+    def test_get_logr_for_uncovered_position_raises_exception(self):
+        with self.assertRaises(UncoveredPositionError):
+            self.segmentation.logr(chromosome='chr2', position=100)
