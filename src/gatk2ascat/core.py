9e9567411617da619c7149c22e67eb78d3047f16
Michael Knudsen <micknudsen@gmail.com>
2020-02-18 19:49:47 +0100

    Implemented initializer in Segmentation class

--- src/gatk2ascat/core.py
+++ src/gatk2ascat/core.py
@@ -1,4 +1,8 @@
+from collections import defaultdict
+
+from typing import DefaultDict
 from typing import Iterable
+from typing import List
 from typing import NamedTuple
 
 
@@ -12,4 +16,6 @@ class Segment(NamedTuple):
 class Segmentation:
 
     def __init__(self, segments: Iterable[Segment]) -> None:
-        pass
+        self._segments: DefaultDict[str, List[Segment]] = defaultdict(list)
+        for segment in segments:
+            self._segments[segment.chromosome].append(segment)
