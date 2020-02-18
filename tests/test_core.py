07aa603dcb08a32650e44bb8363e5e0b26557125
Michael Knudsen <micknudsen@gmail.com>
2020-02-18 19:55:29 +0100

    Refactoring

--- tests/test_core.py
+++ tests/test_core.py
@@ -20,8 +20,8 @@ class TestSegmentation(unittest.TestCase):
         self.segment_1a = Segment(chromosome='chr1', start=100, end=200, logr=0.5)
         self.segment_1b = Segment(chromosome='chr1', start=201, end=350, logr=1.2)
         self.segment_2a = Segment(chromosome='chr2', start=150, end=400, logr=-0.7)
+        self.segmentation = Segmentation(segments=[self.segment_1a, self.segment_1b, self.segment_2a])
 
     def test_create_segmentation(self):
-        segmentation = Segmentation(segments=[self.segment_1a, self.segment_1b, self.segment_2a])
-        self.assertEqual(segmentation._segments['chr1'], [self.segment_1a, self.segment_1b])
-        self.assertEqual(segmentation._segments['chr2'], [self.segment_2a])
+        self.assertEqual(self.segmentation._segments['chr1'], [self.segment_1a, self.segment_1b])
+        self.assertEqual(self.segmentation._segments['chr2'], [self.segment_2a])
