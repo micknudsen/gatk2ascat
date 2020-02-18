67d10e66120f90a014e85691445901ba60b8d960
Michael Knudsen <micknudsen@gmail.com>
2020-02-18 19:44:27 +0100

    Added test for creating Segmentation object

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 import unittest
 
 from gatk2ascat.core import Segment
+from gatk2ascat.core import Segmentation
 
 
 class TestSegment(unittest.TestCase):
@@ -11,3 +12,16 @@ class TestSegment(unittest.TestCase):
         self.assertEqual(segment.start, 100)
         self.assertEqual(segment.end, 200)
         self.assertEqual(segment.logr, 0.5)
+
+
+class TestSegmentation(unittest.TestCase):
+
+    def setUp(self):
+        self.segment_1a = Segment(chromosome='chr1', start=100, end=200, logr=0.5)
+        self.segment_1b = Segment(chromosome='chr1', start=201, end=350, logr=1.2)
+        self.segment_2a = Segment(chromosome='chr2', start=150, end=400, logr=-0.7)
+
+    def test_create_segmentation(self):
+        segmentation = Segmentation(segments=[self.segment_1a, self.segment_1b, self.segment_2a])
+        self.assertEqual(segmentation._segments['chr1'], [self.segment_1a, self.segment_1b])
+        self.assertEqual(segmentation._segments['chr2'], [self.segment_2a])
