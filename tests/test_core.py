da21b2d57863502a2e4c1c64ca6a414ca2ab0e49
Michael Knudsen <micknudsen@gmail.com>
2020-02-18 19:15:39 +0100

    Added test for segment class

---
+++ tests/test_core.py
@@ -0,0 +1,13 @@
+import unittest
+
+from gatk2ascat.core import Segment
+
+
+class TestCore(unittest.TestCase):
+
+    def test_segment(self):
+        segment = Segment(chromosome='chr2', start=100, end=200, logr=0.5)
+        self.assertEqual(segment.chromosome, 'chr2')
+        self.assertEqual(segment.start, 100)
+        self.assertEqual(segment.end, 200)
+        self.assertEqual(segment.logr, 0.5)
