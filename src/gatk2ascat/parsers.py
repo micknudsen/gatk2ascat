9a83f5a930cd9dcd40440d4b2cb1735fc251805c
Michael Knudsen <micknudsen@gmail.com>
2020-02-18 20:36:45 +0100

    Implemented parse_segments function

--- src/gatk2ascat/parsers.py
+++ src/gatk2ascat/parsers.py
@@ -1,7 +1,25 @@
 from typing import Iterable
+from typing import List
 
+from gatk2ascat.core import Segment
 from gatk2ascat.core import Segmentation
 
 
 def parse_segments(stream: Iterable[str]) -> Segmentation:
-    pass
+
+    segments: List[Segment] = []
+
+    skipped_header = False
+    for line in stream:
+        if line.startswith('@'):
+            continue
+        if not skipped_header:
+            skipped_header = True
+            continue
+        chromosome, start, end, logr = line.split('\t')
+        segments.append(Segment(chromosome=chromosome,
+                                start=int(start),
+                                end=int(end),
+                                logr=float(logr)))
+
+    return Segmentation(segments=segments)
