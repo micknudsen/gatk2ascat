ee3ab03388865738a1a0605ca92ce8d58e507f6a
Michael Knudsen <micknudsen@gmail.com>
2020-02-19 10:48:46 +0100

    Refactoring

--- src/gatk2ascat/parsers.py
+++ src/gatk2ascat/parsers.py
@@ -25,22 +25,9 @@ def parse_segments(stream: Iterator[str]) -> Segmentation:
 
     segments: List[Segment] = []
 
-    # Make sure not to try to parse column names line.
-    skipped_column_names_line = False
+    skip_header(stream)
 
     for line in stream:
-
-        # Skip the SAM-style header lines.
-        if line.startswith('@'):
-            continue
-
-        # This first line after the SAM-style header is the
-        # column names lines. Skip that one, too.
-        if not skipped_column_names_line:
-            skipped_column_names_line = True
-            continue
-
-        # All remaining lines correspond to a segment.
         chromosome, start, end, logr = line.split('\t')
         segment = Segment(chromosome=chromosome, start=int(start), end=int(end), logr=float(logr))
         segments.append(segment)
@@ -52,22 +39,9 @@ def parse_snps(stream: Iterator[str]) -> List[SNP]:
 
     snps: List[SNP] = []
 
-    # Make sure not to try to parse column names line.
-    skipped_column_names_line = False
+    skip_header(stream)
 
     for line in stream:
-
-        # Skip the SAM-style header lines.
-        if line.startswith('@'):
-            continue
-
-        # This first line after the SAM-style header is the
-        # column names lines. Skip that one, too.
-        if not skipped_column_names_line:
-            skipped_column_names_line = True
-            continue
-
-        # All remaining lines correspond to a SNP.
         chromosome, position, ref_count, alt_count, *_ = line.split('\t')
         baf = float(alt_count) / (float(ref_count) + float(alt_count))
         snp = SNP(chromosome=chromosome, position=int(position), baf=baf)
