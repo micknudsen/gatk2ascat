66d0f83c06e1099cbb7a05112565415443973013
Michael Knudsen <micknudsen@gmail.com>
2020-02-19 11:16:51 +0100

    Parse GATK output files

--- src/gatk2ascat/client.py
+++ src/gatk2ascat/client.py
@@ -1,6 +1,7 @@
 import argparse
 
 from gatk2ascat.parsers import parse_segments
+from gatk2ascat.parsers import parse_snps
 
 
 def main():
@@ -15,3 +16,14 @@ def main():
 
     with open(args.denoised_copy_ratios, 'r') as f:
         segmentation = parse_segments(stream=f)
+
+    with open(args.allelic_counts_tumor, 'r') as f:
+        tumor_baf = parse_snps(stream=f)
+
+    with open(args.allelic_counts_normal, 'r') as f:
+        normal_baf = parse_snps(stream=f)
+
+    # Just to silence flake8 while developing!
+    print(segmentation)
+    print(tumor_baf)
+    print(normal_baf)
